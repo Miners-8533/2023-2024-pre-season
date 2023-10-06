6c8fc47d34150cc828402d4fe4bd3ac1a3a782a1
Jack Jenkins <jack@brightbraindesign.com>
2023-10-06 13:43:39 -0400

    fix: tuning session with spring-loaded chain tensioners

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 10.25; // in
+    public static double TRACK_WIDTH = 10.3; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.01095;
+    public static double kV = 0.01311;
     public static double kA = 0.00225;
-    public static double kStatic = 0.06178;
+    public static double kStatic = 0.12276;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -66,7 +66,7 @@ public class DriveConstants {
      * inches.
      */
     public static double MAX_VEL = 25;
-    public static double MAX_ACCEL = 60;
+    public static double MAX_ACCEL = 30;
     public static double MAX_ANG_VEL = 2.72;
     public static double MAX_ANG_ACCEL = Math.toRadians(90);
 
